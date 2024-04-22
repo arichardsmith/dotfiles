492faf3b55309d99ae1c8114ae552320de77a347
Richard Smith <richardmcsmith@gmail.com>
2024-04-22 14:23:20 +0900

    Add alias for eza

--- tools/zsh/aliases.sh
+++ tools/zsh/aliases.sh
@@ -1,3 +1,4 @@
 # Add custom aliases here if need, but prefer writing them as scripts in ../scripts
 alias ungron="gron --ungron"
 alias crl="curlie"
+alias ls="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
