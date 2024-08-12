8badf4ba1fdd6d073ef0f321186b46875a4b7509
Richard Smith <richardmcsmith@gmail.com>
2024-08-12 13:05:44 +0900

    Add raycast script commands

---
+++ raycast/scripts/aerospace-toggle.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+# Raycast Script Command Template
+#
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Toggle Aerospace
+# @raycast.mode silent
+#
+# Optional parameters:
+# @raycast.icon 🪟
+
+aerospace enable toggle
