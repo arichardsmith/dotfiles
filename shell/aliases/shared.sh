7019ceacc35b3b40c82fd72d8aa693e1f30f0935
Richard Smith <richardmcsmith@gmail.com>
2024-08-16 14:41:34 +0900

    Add new git aliases

--- shell/aliases/shared.sh
+++ shell/aliases/shared.sh
@@ -2,4 +2,7 @@
 alias readit="bat --style=plain,header readme.md"
 alias gp="git push"
 alias gc="git-commit-edit"
+alias gca="git-add-commit"
 alias ga="git add ."
+alias gsw="git-switch-pick"
+alias gswc="git switch --create"
