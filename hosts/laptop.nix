4ad90d0d4bc501010bbfd8640c1edf8a1edf5ff7
Richard Smith <richardmcsmith@gmail.com>
2025-08-23 10:17:00 +0900

    Add nix init to zshrc on laptop

--- hosts/laptop.nix
+++ hosts/laptop.nix
@@ -14,11 +14,22 @@
     home.sessionVariables = {
       DOCKER_HOST = "unix://$HOME/.colima/docker.sock";
       EDITOR = "nvim";
+      EDITOR_UI = "zed"; # Prefered IDE / UI based editor
     };
 
     zsh.aliases = {
       tailscale = "/Applications/Tailscale.app/Contents/MacOS/Tailscale";
     };
+
+    # Updating macos can clobber the nix additions to /etc/zshrc, so make sure they are added
+    # to the user .zshrc
+    zsh.initContent = [
+      ''
+        if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+            . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+        fi
+      ''
+    ];
 
     programs.starship.settings = {
       hostname = {
