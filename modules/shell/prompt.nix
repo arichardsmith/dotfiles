7b379bb99eb89b96d6d05f725806898d7807889b
Richard Smith <richardmcsmith@gmail.com>
2025-08-31 17:09:50 +0900

    Add catppuccin theme for prompt

--- modules/shell/prompt.nix
+++ modules/shell/prompt.nix
@@ -32,11 +32,42 @@ in {
 
         right_format = "$cmd_duration";
 
+        palette = "catppuccin_mocha";
+
+        palettes.catppuccin_mocha = {
+          rosewater = "#f5e0dc";
+          flamingo = "#f2cdcd";
+          pink = "#f5c2e7";
+          mauve = "#cba6f7";
+          red = "#f38ba8";
+          maroon = "#eba0ac";
+          peach = "#fab387";
+          yellow = "#f9e2af";
+          green = "#a6e3a1";
+          teal = "#94e2d5";
+          sky = "#89dceb";
+          sapphire = "#74c7ec";
+          blue = "#89b4fa";
+          lavender = "#b4befe";
+          text = "#cdd6f4";
+          subtext1 = "#bac2de";
+          subtext0 = "#a6adc8";
+          overlay2 = "#9399b2";
+          overlay1 = "#7f849c";
+          overlay0 = "#6c7086";
+          surface2 = "#585b70";
+          surface1 = "#45475a";
+          surface0 = "#313244";
+          base = "#1e1e2e";
+          mantle = "#181825";
+          crust = "#11111b";
+        };
+
         directory = {
           format = "[$path]($style)";
-          style = "bold bright-blue";
-          repo_root_style = "bold bright-blue";
-          before_repo_root_style = "white";
+          style = "bold blue";
+          repo_root_style = "bold blue";
+          before_repo_root_style = "text";
           truncation_length = 2;
           truncation_symbol = "…/";
           truncate_to_repo = false;
@@ -49,22 +80,22 @@ in {
 
         username = {
           format = "[$user@]($style)";
-          style_user = "white";
+          style_user = "text";
         };
 
         hostname = {
           format = "[$hostname]($style)";
-          style = "white";
+          style = "text";
           ssh_only = false;
         };
 
         cmd_duration = {
-          format = "[$duration](black)";
+          format = "[$duration](overlay0)";
         };
       } // lib.optionalAttrs jjEnabled {
         custom.jj = {
           command = "starship-jj --ignore-working-copy starship prompt";
-          format = " [jj:$output](bright-black)";
+          format = " [jj:$output](overlay1)";
           ignore_timeout = true;
           # Only show if we are in a jj repo
           when = "jj --ignore-working-copy root";
