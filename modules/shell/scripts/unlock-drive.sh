2ad22db7859556ccd901e634dd71adc2ff1af867
Richard Smith <richardmcsmith@gmail.com>
2025-11-01 11:22:14 +0900

    Update shell script error behaviour

--- modules/shell/scripts/unlock-drive.sh
+++ modules/shell/scripts/unlock-drive.sh
@@ -1,4 +1,7 @@
-#!/bin/zsh
+#!/usr/bin/env zsh
+set -e  # Exit immediately if any command fails
+set -u  # Exit if referencing undefined variables
+
 SERVICE_NAME="TimeMachineDrive"
 PASSPHRASE=$(security find-generic-password -a "$USER" -s "$SERVICE_NAME" -w)
 echo "$PASSPHRASE" | diskutil apfs unlockVolume disk6s2 -stdinpassphrase
