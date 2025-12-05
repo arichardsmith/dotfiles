18abb8110ae2aa9b50b33250d63ade11a2e882e7
Richard Smith <richardmcsmith@gmail.com>
2025-12-05 09:39:20 +0900

    Use the general name for the time machine volume
    The direct ID can change

--- modules/shell/scripts/unlock-drive.sh
+++ modules/shell/scripts/unlock-drive.sh
@@ -3,5 +3,6 @@ set -e  # Exit immediately if any command fails
 set -u  # Exit if referencing undefined variables
 
 SERVICE_NAME="TimeMachineDrive"
+DRIVE_NAME="Time Machine"
 PASSPHRASE=$(security find-generic-password -a "$USER" -s "$SERVICE_NAME" -w)
-echo "$PASSPHRASE" | diskutil apfs unlockVolume disk6s2 -stdinpassphrase
+echo "$PASSPHRASE" | diskutil apfs unlockVolume "$DRIVE_NAME" -stdinpassphrase
