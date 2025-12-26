e2aa787cf95cb18e730d26ebc86f6c9093485203
Richard Smith <richardmcsmith@gmail.com>
2025-12-26 16:23:52 +0900

    Add a pack for server tools

--- packs/default.nix
+++ packs/default.nix
@@ -3,11 +3,13 @@
     shell.enable = lib.mkEnableOption "shell pack (terminal utilities, zsh, starship, bat)";
     system.enable = lib.mkEnableOption "system monitoring pack (btop etc)";
     dev.enable = lib.mkEnableOption "dev pack (git, jujutsu, gh, docker)";
+    server.enable = lib.mkEnableOption "server pack (shpool, docker etc)";
   };
 
   imports = [
     ./shell
     ./system
     ./dev
+    ./server
   ];
 }
---
+++ packs/server/default.nix
@@ -0,0 +1,28 @@
+# Server Pack
+# Tools that are useful on remote servers.
+# Includes: shpool for detaching shell sessions
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  cfg = config.packs.server;
+
+  linuxOnlyPackages = with pkgs;
+    lib.optionals (lib.elem pkgs.system ["aarch64-linux" "i686-linux" "x86_64-linux"]) [
+      shpool # A more light weight tmux
+    ];
+in {
+  config = lib.mkIf cfg.enable {
+    customPrograms = {
+      astGrep.enable = true;
+      docker.enable = true;
+    };
+
+    home.packages =
+      [
+      ]
+      ++ linuxOnlyPackages;
+  };
+}
