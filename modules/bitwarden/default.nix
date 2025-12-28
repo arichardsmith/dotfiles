7515f7811df85b36875220398631c5d9a69945d7
Richard Smith <richardmcsmith@gmail.com>
2025-12-28 17:15:02 +0900

    Only create the rbw config if it's enabled

--- modules/bitwarden/default.nix
+++ modules/bitwarden/default.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: {
   config = {
@@ -13,10 +14,16 @@
     # The file produced by rbw.settings is invalid if we don't set pinentry, and the pinentry package doesn't work on Darwin!
     # We have to resort to manually setting the config like cavemen.
 
-    home.file."Library/Application Support/rbw/config.json" = lib.mkForce {
+    home.file."Library/Application Support/rbw/config.json" = lib.mkIf (config.programs.rbw.enable && pkgs.stdenv.isDarwin) (lib.mkForce {
       text = builtins.toJSON {
         email = config.user.email;
       };
-    };
+    });
+
+    home.file.".config/rbw/config.json" = lib.mkIf (config.programs.rbw.enable && pkgs.stdenv.isLinux) (lib.mkForce {
+      text = builtins.toJSON {
+        email = config.user.email;
+      };
+    });
   };
 }
