66845cfa876fc91cc2b91727da0e13b02e88b4e6
Richard Smith <richardmcsmith@gmail.com>
2025-12-28 17:42:57 +0900

    Set up the git credential helper correctly

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -39,21 +39,12 @@
           root = "rev-parse --show-toplevel";
           hist = "log --graph --abbrev-commit --decorate --date=short --format=format:'%C(bold cyan)%h%C(reset) %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(bold yellow)%d%C(reset)' --branches --remotes --tags";
         };
+      };
+    };
 
-        credential."https://github.com" = {
-          useHttpPath = true;
-          helper = [
-            "" # Clear existing helpers
-            "!/opt/homebrew/bin/gh auth git-credential"
-          ];
-        };
-
-        credential."https://gist.github.com" = {
-          helper = [
-            "" # Clear existing helpers
-            "!/opt/homebrew/bin/gh auth git-credential"
-          ];
-        };
+    programs.gh = {
+      gitCredentialHelper = {
+        enable = true;
       };
     };
   };
