48fead4782aa6d0e0195caade4796c6b32d52d0d
Richard Smith <richardmcsmith@gmail.com>
2026-04-06 17:41:25 +0900

    Add script to keep nix build cache warm on NAS

--- machines/mininas/default.nix
+++ machines/mininas/default.nix
@@ -1,4 +1,5 @@
-{mkHomeConfig}: mkHomeConfig "x86_64-linux" ({...}: {
+{mkHomeConfig}:
+mkHomeConfig "x86_64-linux" ({pkgs, ...}: {
   imports = [./programs.nix];
 
   config = {
@@ -16,5 +17,29 @@
       "[╭─ ](overlay0)$username$hostname $env_var$line_break"
       + "[├╌ ](overlay0)$directory$\{custom.jj\}$line_break"
       + "[$character ](overlay0)";
+
+    # We have the nas periodically fetch this repo and build it,
+    # so when we do come to updating, it's not as long a process.
+
+    systemd.user.services.hm-prebuild = {
+      Unit.Description = "Pre-build Home Manager generation";
+      Service = {
+        Type = "oneshot";
+        ExecStart = toString (pkgs.writeShellScript "hm-prebuild" ''
+          cd /home/richard/env
+          ${pkgs.git}/bin/git pull --ff-only
+          ${pkgs.nix}/bin/nix build .#homeConfigurations.mininas.activationPackage --no-link
+        '');
+      };
+    };
+
+    systemd.user.timers.hm-prebuild = {
+      Unit.Description = "Periodically pre-build Home Manager";
+      Timer = {
+        OnCalendar = "daily";
+        Persistent = true;
+      };
+      Install.WantedBy = ["timers.target"];
+    };
   };
 })
