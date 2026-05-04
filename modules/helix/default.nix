3a7997ce1e94677828ea935a880220b248e15fb4
Richard Smith <richardmcsmith@gmail.com>
2026-05-04 13:27:07 +0900

    Add vim style aliases for line end / start

--- modules/helix/default.nix
+++ modules/helix/default.nix
@@ -2,6 +2,16 @@
   config.programs.helix = {
     settings = {
       theme = "catppuccin_macchiato";
+      keys = {
+        normal = {
+          "$" = "goto_line_end";
+          "^" = "goto_line_start";
+        };
+        select = {
+          "$" = "goto_line_end";
+          "^" = "goto_line_start";
+        };
+      };
     };
   };
 }
