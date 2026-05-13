e34c0abfa57aea44ce647a31d3aaf6140fdf2498
Richard Smith <richardmcsmith@gmail.com>
2026-05-13 12:52:20 +0900

    Change which ssh key gets unlocked

--- machines/laptop/scripts/unlock-ssh.sh
+++ machines/laptop/scripts/unlock-ssh.sh
@@ -1,5 +1,5 @@
 # Unlock the daily ssh key so we can ssh into servers without entering a password
-DAILY_SSH_KEY="${DAILY_SSH_KEY:-$HOME/.ssh/daily_key}"
+DAILY_SSH_KEY="${DAILY_SSH_KEY:-$HOME/.ssh/laptop}"
 if ! ssh-add -l | grep -q "$(ssh-keygen -lf "$DAILY_SSH_KEY" 2>/dev/null | awk '{print $2}')"; then
   ssh-add "$DAILY_SSH_KEY"
 fi
