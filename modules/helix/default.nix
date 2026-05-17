74377bd95b874ec91b57130085a49aef6ac3320f
Richard Smith <richardmcsmith@gmail.com>
2026-05-17 09:42:15 +0900

    Enable home-row movements in insert mode in helix

--- modules/helix/default.nix
+++ modules/helix/default.nix
@@ -8,6 +8,13 @@
           "^" = "goto_line_start";
           "C-r" = ":reload";
         };
+        insert = {
+          # Enable home-row movements in insert mode
+          "C-h" = "move_char_left";
+          "C-l" = "move_char_right";
+          "C-j" = "move_visual_line_down";
+          "C-k" = "move_visual_line_up";
+        };
         select = {
           "$" = "goto_line_end";
           "^" = "goto_line_start";
