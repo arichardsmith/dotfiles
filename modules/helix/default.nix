4b94e4500ba9875a131022b9b5ae8f88e268e089
Richard Smith <richardmcsmith@gmail.com>
2026-05-20 22:15:49 +0900

    Add some more helix keybindings

--- modules/helix/default.nix
+++ modules/helix/default.nix
@@ -7,6 +7,11 @@
           "$" = "goto_line_end";
           "^" = "goto_line_start";
           "C-r" = ":reload";
+
+          space = {
+            d = ":cd %sh{dirname '%{buffer_name}'}";
+            D = '':cd %sh{jj root 2>/dev/null || git rev-parse --show-toplevel}'';
+          };
         };
         insert = {
           # Enable home-row movements in insert mode
@@ -14,6 +19,7 @@
           "C-l" = "move_char_right";
           "C-j" = "move_visual_line_down";
           "C-k" = "move_visual_line_up";
+          "C-o" = "open_below";
         };
         select = {
           "$" = "goto_line_end";
