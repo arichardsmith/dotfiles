5101e51d21713bfff9656905dc4a1ad48e3d6fce
Richard Smith <richardmcsmith@gmail.com>
2026-06-08 18:03:17 +0900

    Update helix keybindings

--- modules/helix/default.nix
+++ modules/helix/default.nix
@@ -25,15 +25,21 @@
             d = ":cd %sh{dirname '%{buffer_name}'}";
             D = '':cd %sh{jj root 2>/dev/null || git rev-parse --show-toplevel}'';
           };
+
+          # Treat backslash as an alternative to not being able to define custom commands
+          "\\" = {
+            d = ":sh rm %{buffer_name}";
+            D = [":sh rm %{buffer_name}" ":buffer-close"];
+            f = ":format";
+            w = ":toggle-option soft-wrap.enable";
+          };
         };
+
         insert = {
-          # Enable home-row movements in insert mode
-          "C-h" = "move_char_left";
-          "C-l" = "move_char_right";
-          "C-j" = "move_visual_line_down";
-          "C-k" = "move_visual_line_up";
-          "C-o" = "open_below";
+          # Skip past an auto-inserted closing delimiter.
+          "C-;" = "move_char_right";
         };
+
         select = {
           "$" = "goto_line_end";
           "^" = "goto_line_start";
