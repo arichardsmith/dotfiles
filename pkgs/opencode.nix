59b7d16293881b3a233c4c4e4d11cfb05f6221bb
Richard Smith <richardmcsmith@gmail.com>
2026-06-18 12:16:48 +0900

    Update opencode

--- pkgs/opencode.nix
+++ pkgs/opencode.nix
@@ -1,15 +1,15 @@
 { stdenv, fetchurl }:
 let
-  version = "1.17.3";
+  version = "1.17.8";
 
   sources = {
     "aarch64-darwin" = {
       url = "https://registry.npmjs.org/opencode-darwin-arm64/-/opencode-darwin-arm64-${version}.tgz";
-      hash = "sha256-dzMX8SJfiRjYGduvPRElo7PMWFqx6YKz/deIGESiEso=";
+      hash = "sha256-n9mRynW4IdDrIPbmR2m8hui/OTqHbGDheWa+KiPKCtc=";
     };
     "x86_64-linux" = {
       url = "https://registry.npmjs.org/opencode-linux-x64/-/opencode-linux-x64-${version}.tgz";
-      hash = "sha256-HGoziSxZhGoJkEmITKky0NYjNFoewo6YiWIUWARNq2o=";
+      hash = "sha256-YZ7Gfd4pklo2HM+TGMd/3isFmheoRB9zBQ6yj52/5hA=";
     };
   };
 in stdenv.mkDerivation {
