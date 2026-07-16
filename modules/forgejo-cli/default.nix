a2c58e54efcd53973ead1d8e8d33b5bdbbad47dd
Richard Smith <richardmcsmith@gmail.com>
2026-07-16 20:57:29 +0900

    Work around fj remote issue

--- modules/forgejo-cli/default.nix
+++ modules/forgejo-cli/default.nix
@@ -7,5 +7,11 @@
 helpers.mkProgram {inherit config pkgs;} "forgejoCli" {
   setup = {pkgs, ...}: {
     home.packages = [pkgs.forgejo-cli];
+
+    my.shell.aliases = {
+      # Jujutsu's detached Git HEAD prevents fj from finding the Forgejo remote when multiple remotes exist.
+      # I only use fj with the instance on mininas, so hard coding the host will work around this.
+      fj = "fj --host git.mininas.sanroku.dev";
+    };
   };
 }
